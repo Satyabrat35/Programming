166b8aa7290ffa8aa379cc57689d476ef9ec7a91
Satyabrat35 <satyabrat35@gmail.com>
2018-12-18 17:00:34 +0530

    unit tests

--- String/hk2.cpp
+++ String/hk2.cpp
@@ -14,42 +14,37 @@ typedef unsigned long long int ull;
 #define minm(a,b,c)  min(a,min(c,b))
 #define f(i,n)  for(int i=1;i<n;i++)
 #define rf(i,n) for(int i=n-1;i>=0;i--)
-int comham(string s1,string s2){
-    int i=0,ct=0;
-    while(s1[i]!='\0'){
-        if(s1[i]!=s2[i]){
-            ct++;
-        }
-        i++;
-    }
-    return ct;
-}
 int main()
 {
-    int n,m;
-    cin>>n>>m;
-    int a[n];
-    vector<string> s;
-    memset(a,0,sizeof(a));
-    for(int i=0;i<n;i++){
-        string b;
-        cin>>b;
-        s.push_back(b);
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            int x = comham(s[i],s[j]);
-            a[i]+=x;
+    xyz;
+    string s;
+    vector<int> pos;
+    cin>>s;
+    int l = s.length();
+    int x=-1,ct=0;
+    for(int i=0;i<l;i++){
+        if(x<s[i]){
+            x=s[i];
         }
     }
-    int mn=1e5,pos=-1;
-    for(int i=0;i<n;i++){
-        //cout<<a[i]<<endl;
-        if(mn>a[i]){
-            mn=a[i];
-            pos=i;
+    for(int i=0;i<l;i++){
+        if(x==s[i]){
+            ct++;
+            pos.push_back(i);
         }
     }
-    cout<<s[pos];
+    if(ct==1){
+        int p = pos[0];
+        for(int i=p;i<l;i++){
+            cout<<s[i];
+        }
+        for(int i=0;i<p;i++){
+            cout<<s[i];
+        }
+    }
+    else {
+        
+    }
+
     return 0;
 }
