3fc09ece796e7b63919f6cfe6583412cde4c23e7
Satyabrat35 <satyabrat35@gmail.com>
2019-08-08 22:05:11 +0530

    practice

--- gfg/first.cpp
+++ gfg/first.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
-bool pal(string z){
-    int l = 0;
-    int h = z.size()-1;
-    while(l<h){
-        if(z[l++]!=z[h--]){
-            return false;
-        }
-    }
-    return true;
-}
-int sub(string s, int n){
-    int l = 0;
-    for(int i=0;i<n;i++){
-        for(int j=i;j<=n-i;j++){
-            string k = s.substr(i,j);
-            if(pal(k)==true){
-                int q = k.size();
-                l = max(l , q);
-            }
-        }
-    }
-    return l;
-}
 int main() {
 	//code
 	int t;
 	cin>>t;
 	while(t--){
-	    string s;
-	    cin>>s;
-	    cout<<sub(s,s.size())<<endl;
+	    int n,m;
+        cin>>n>>m;
+        int a[n];
+        for(int i=0;i<n;i++)cin>>a[i];
+        int dp[n+1][m+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=0;i<=n;i++)dp[i][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(j>=a[i-1]){
+                    dp[i][j] = dp[i-1][j]+dp[i-1][j-a[i-1]];
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        cout<<dp[n][m];
 	}
 	return 0;
 }
